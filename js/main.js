04ba4b7ea918b3794db3d4377becd12a842a2c93
leozaviriukha <leozaviriukha@ukr.net>
2020-10-24 20:05:06 +0300

    add aside-filter__item-drop with rangeSider

--- js/main.js
+++ js/main.js
@@ -36,5 +36,12 @@ $(function(){
     $(this).next().slideToggle('200');
   });
 
+  
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 100000,
+    max: 500000,
+  });
+
 });
 
